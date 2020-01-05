e255e6d9c820d0eb52e2705c02d9f0bdd7d3de7e
osddeitf <osddeitf@gmail.com>
2020-01-05 10:45:10 +0700

    Added game menu, buttons and worldmap scene

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Sprite hoverTexture;
+    public Sprite downTexture;
+    SpriteRenderer render;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+        render.sprite = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseEnter() {
+        if (render.sprite == null)
+            render.sprite = hoverTexture;
+    }
+
+    void OnMouseExit() {
+        if (render.sprite == hoverTexture)
+            render.sprite = null;
+    }
+
+    void OnMouseDown() {
+        render.sprite = downTexture;
+    }
+
+    void OnMouseUp() {
+        render.sprite = null;
+    }
+
+    void OnMouseUpAsButton() {
+        Debug.Log("Button clicked");
+    }
+}
